6bf1c766990b77e846fec7e32b6748d99033529f
jtrautma <33090567+jtrautma@users.noreply.github.com>
2023-12-27 18:28:17 +0000

    Update Chapter4_Exercise1_SumOfRange.js

--- Chapter4_Exercise1_SumOfRange.js
+++ Chapter4_Exercise1_SumOfRange.js
@@ -1,20 +1,24 @@
 // Your code here.
-function rangeSimple(start, end) {
+function range(start, end, step) {
   var rangeArray = []
-  for(var i = start; i <= end; i++) {
-    rangeArray.push(i);}
-  return rangeArray
+  if (arguments.length < 3) {
+    var step = 1
+      }
+  console.log("step is", step)
+  for(var j = start; j <= end; j += step) {
+    rangeArray.push(j);
+     }
+    return rangeArray
 }
 
-function rangeStep(start, end, step) {
-  if arguments.length < 3
-
+function sum(start, end) {
+  console.log("let's sum it up")
+  //range(start, end)
   }
 
-console.log(rangeSimple(1, 10));
-console log(rangeSimple(1, 10));
+console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-//console.log(range(5, 2, -1));
+console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
-//console.log(sum(range(1, 10)));
+console.log(sum(range(1, 10)));
 // → 55
