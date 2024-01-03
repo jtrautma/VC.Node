320b8a7783909f8fe623f315971c6d4567089dc8
jtrautma <33090567+jtrautma@users.noreply.github.com>
2024-01-03 12:35:43 +0000

    My final solution

--- Chapter4_Exercise2_ReversingArray.js
+++ Chapter4_Exercise2_ReversingArray.js
@@ -1,10 +1,4 @@
 // Your code here.
-
-
-
-
-
-// Your code here.
 function reverseArray(array) {
   var modArray = []
   for(var i = array.length-1; i >= 0; i--) {
@@ -26,11 +20,4 @@ console.log(reverseArray(["A", "B", "C"]));
 
 var arrayValue = [1, 2, 3, 4, 5];
 console.log(reverseArrayInPlace(arrayValue));
-// → [5, 4, 3, 2, 1]
-
-console.log(reverseArray(["A", "B", "C"]));
-// → ["C", "B", "A"];
-var arrayValue = [1, 2, 3, 4, 5];
-reverseArrayInPlace(arrayValue);
-console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
